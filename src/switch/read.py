ec5063429832120f713ad3a0d48ee0296a2ee4ad
Zdenek Philipp <zdenek.philipp@firma.seznam.cz>
2014-02-07 08:43:23 +0100

    experimenty

---
+++ src/switch/read.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import serial
+
+SERIAL  = "/dev/tty.NoZAP-PL2303-000013FA"
+
+ser = serial.Serial(SERIAL, 9600)
+
+def readData():
+    print ser.readline()
+
+while 1:
+    readData()
